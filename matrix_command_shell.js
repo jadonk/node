f9bca610d8c6f24bb7d2b71cb317abd56fed64ef
Jason Kridner <jdk@ti.com>
2010-04-09 10:47:09 -0400

    Added simply HTML file server.

--- matrix_command_shell.js
+++ matrix_command_shell.js
@@ -4,6 +4,7 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var child_process = require('child_process');
+var path = require('path');
 
 // Spawn child process
 child_process.spawn('mkfifo', ['matrix_pipe']);
@@ -18,17 +19,46 @@ child.stdout.addListener(
 );
 
 // Serve web page and notify user
+function loadHTMLFile(uri, res) {
+ var filename = path.join(process.cwd(), uri);
+ path.exists(
+  filename,
+  function(exists) {
+   if(!exists) {
+    res.sendHeader(404, {"Content-Type": "text/plain"});
+    res.write("404 Not Found\n");
+    res.close();
+    return;
+   }
+   fs.readFile(
+    filename,
+    "text",
+    function(err, file) {
+     if(err) {
+      res.sendHeader(500, {"Content-Type": "text/plain"});
+      res.write(err + "\n");
+      res.close();
+      return;
+     }
+     res.sendHeader(200, {"Content-Type": "text/html"});
+     res.write(file, "text");
+     res.close();
+    }
+   );
+  }
+ );
+}
 http.createServer(
  function (req, res) {
   var uri = url.parse(req.url).pathname;
   if(uri == '/') {
+   loadHTMLFile('/index.html', res);
+  } else if(uri == '/data') {
    res.writeHead(200, {'Content-Type': 'text/plain'});
    res.write(matrix_data);
    res.close();
   } else {
-   res.writeHead(404, {'Content-Type': 'text/plain'});
-   res.write('File not found');
-   res.close();
+   loadHTMLFile(uri, res);
   }
  }
 ).listen(8000);
