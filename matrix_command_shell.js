9207311bc7ab0b67d9d4cc2cce0ca86dd66f22de
Jason Kridner <jdk@ti.com>
2010-04-09 09:16:57 -0400

    Removed commented out lines.

--- matrix_command_shell.js
+++ matrix_command_shell.js
@@ -2,20 +2,8 @@ var sys = require('sys');
 var http = require('http');
 var fs = require('fs');
 var child_process = require('child_process');
-//sys.exec('mkfifo matrix_pipe');
 child_process.spawn('mkfifo', ['matrix_pipe']);
-//sys.exec('echo "#!/bin/sh" > matrix_pipe.sh');
-//sys.exec('echo "cat matrix_pipe | cat > matrix_pipe.txt &" >> matrix_pipe.sh');
-//fs.chmodSync('./matrix_pipe.sh', '+x');
-//sys.exec('./matrix_pipe.sh');
 var child = child_process.spawn('cat', ['matrix_pipe']);
-//var matrix_data = fs.readFileSync('./matrix_pipe.txt');
-//fs.watchFile(
-// 'matrix_pipe.txt',
-// function (curr, prev) {
-//  matrix_data = fs.readFileSync('./matrix_pipe.txt');
-// }
-//);
 var matrix_data = '';
 child.stdout.addListener(
  'data',
