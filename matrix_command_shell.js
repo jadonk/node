e6354afa58cfbe1f3c62b9b1c2246366fd4f01de
Jason Kridner <jdk@ti.com>
2010-04-09 09:59:58 -0400

    Added a basic URL parser.

--- matrix_command_shell.js
+++ matrix_command_shell.js
@@ -1,7 +1,9 @@
 var sys = require('sys'); 
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
 var child_process = require('child_process');
+
 child_process.spawn('mkfifo', ['matrix_pipe']);
 var child = child_process.spawn('cat', ['matrix_pipe']);
 var matrix_data = '';
@@ -14,9 +16,16 @@ child.stdout.addListener(
 );
 http.createServer(
  function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write(matrix_data);
-  res.close();
+  var uri = url.parse(req.url).pathname;
+  if(uri == '/') {
+   res.writeHead(200, {'Content-Type': 'text/plain'});
+   res.write(matrix_data);
+   res.close();
+  } else {
+   res.writeHead(404, {'Content-Type': 'text/plain'});
+   res.write('File not found');
+   res.close();
+  }
  }
 ).listen(8000);
 sys.puts('Server running at http://127.0.0.1:8000/');
